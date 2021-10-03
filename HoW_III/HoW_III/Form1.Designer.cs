d8bd22a9ec925ad154463c5adf1ee44999376b57
eliassilveirajr <84023956+eliassilveirajr@users.noreply.github.com>
2021-10-03 19:31:27 -0300

    Add files via upload

---
+++ HoW_III/HoW_III/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿
+namespace HoW_III
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.calendario = new System.Windows.Forms.MonthCalendar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.tbEndereco = new System.Windows.Forms.RichTextBox();
+            this.dgAgenda = new System.Windows.Forms.DataGridView();
+            this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnTelefone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnHora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnData = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnEndereco = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.tbTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbHora = new System.Windows.Forms.MaskedTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgAgenda)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // calendario
+            // 
+            this.calendario.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.calendario.Location = new System.Drawing.Point(378, 110);
+            this.calendario.MaxSelectionCount = 1;
+            this.calendario.MinDate = new System.DateTime(2021, 10, 3, 0, 0, 0, 0);
+            this.calendario.Name = "calendario";
+            this.calendario.TabIndex = 5;
+            this.calendario.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.calendario_DateChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID:";
+            // 
+            // tbId
+            // 
+            this.tbId.Enabled = false;
+            this.tbId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbId.Location = new System.Drawing.Point(93, 18);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(30, 26);
+            this.tbId.TabIndex = 0;
+            this.tbId.TabStop = false;
+            // 
+            // tbEndereco
+            // 
+            this.tbEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEndereco.Location = new System.Drawing.Point(16, 183);
+            this.tbEndereco.Name = "tbEndereco";
+            this.tbEndereco.Size = new System.Drawing.Size(264, 99);
+            this.tbEndereco.TabIndex = 3;
+            this.tbEndereco.Text = "";
+            // 
+            // dgAgenda
+            // 
+            this.dgAgenda.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dgAgenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgAgenda.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnId,
+            this.ColumnNome,
+            this.ColumnTelefone,
+            this.ColumnHora,
+            this.ColumnData,
+            this.ColumnEndereco});
+            this.dgAgenda.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dgAgenda.Location = new System.Drawing.Point(16, 368);
+            this.dgAgenda.Name = "dgAgenda";
+            this.dgAgenda.ReadOnly = true;
+            this.dgAgenda.Size = new System.Drawing.Size(669, 165);
+            this.dgAgenda.TabIndex = 10;
+            this.dgAgenda.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgAgenda_CellContentClick_1);
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.HeaderText = "Id";
+            this.ColumnId.Name = "ColumnId";
+            this.ColumnId.ReadOnly = true;
+            this.ColumnId.Width = 50;
+            // 
+            // ColumnNome
+            // 
+            this.ColumnNome.HeaderText = "Nome";
+            this.ColumnNome.Name = "ColumnNome";
+            this.ColumnNome.ReadOnly = true;
+            this.ColumnNome.Width = 200;
+            // 
+            // ColumnTelefone
+            // 
+            this.ColumnTelefone.HeaderText = "Telefone";
+            this.ColumnTelefone.Name = "ColumnTelefone";
+            this.ColumnTelefone.ReadOnly = true;
+            // 
+            // ColumnHora
+            // 
+            this.ColumnHora.HeaderText = "Hora";
+            this.ColumnHora.Name = "ColumnHora";
+            this.ColumnHora.ReadOnly = true;
+            this.ColumnHora.Width = 80;
+            // 
+            // ColumnData
+            // 
+            this.ColumnData.HeaderText = "Data";
+            this.ColumnData.Name = "ColumnData";
+            this.ColumnData.ReadOnly = true;
+            this.ColumnData.Width = 80;
+            // 
+            // ColumnEndereco
+            // 
+            this.ColumnEndereco.HeaderText = "Endereço";
+            this.ColumnEndereco.Name = "ColumnEndereco";
+            this.ColumnEndereco.ReadOnly = true;
+            this.ColumnEndereco.Width = 300;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Khaki;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(257, 326);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(99, 27);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // tbNome
+            // 
+            this.tbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNome.Location = new System.Drawing.Point(93, 53);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(187, 26);
+            this.tbNome.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nome:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Telefone:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Endereço:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(374, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Data:";
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnSalvar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(16, 326);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(100, 27);
+            this.btnSalvar.TabIndex = 6;
+            this.btnSalvar.Text = "SALVAR";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // tbData
+            // 
+            this.tbData.Enabled = false;
+            this.tbData.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbData.Location = new System.Drawing.Point(430, 72);
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(138, 26);
+            this.tbData.TabIndex = 19;
+            this.tbData.TabStop = false;
+            // 
+            // tbTelefone
+            // 
+            this.tbTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTelefone.Location = new System.Drawing.Point(93, 97);
+            this.tbTelefone.Mask = "(99) 00000-0000";
+            this.tbTelefone.Name = "tbTelefone";
+            this.tbTelefone.Size = new System.Drawing.Size(187, 31);
+            this.tbTelefone.TabIndex = 2;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnAlterar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlterar.Location = new System.Drawing.Point(137, 326);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(100, 27);
+            this.btnAlterar.TabIndex = 7;
+            this.btnAlterar.Text = "ALTERAR";
+            this.btnAlterar.UseVisualStyleBackColor = false;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(369, 326);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(100, 27);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "EXCLUIR";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(374, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 20);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Hora:";
+            // 
+            // tbHora
+            // 
+            this.tbHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbHora.Location = new System.Drawing.Point(430, 18);
+            this.tbHora.Margin = new System.Windows.Forms.Padding(5, 3, 3, 3);
+            this.tbHora.Mask = "00:00";
+            this.tbHora.Name = "tbHora";
+            this.tbHora.Size = new System.Drawing.Size(57, 29);
+            this.tbHora.TabIndex = 4;
+            this.tbHora.ValidatingType = typeof(System.DateTime);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(697, 545);
+            this.Controls.Add(this.tbHora);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.tbTelefone);
+            this.Controls.Add(this.tbData);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.dgAgenda);
+            this.Controls.Add(this.tbEndereco);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.calendario);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Agenda de reparos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgAgenda)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MonthCalendar calendario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.RichTextBox tbEndereco;
+        private System.Windows.Forms.DataGridView dgAgenda;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.MaskedTextBox tbTelefone;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox tbHora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTelefone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnHora;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEndereco;
+    }
+}
+
---
+++ HoW_III/HoW_III/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HoW_III
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "reparos";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void atualizarGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM agenda";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgAgenda.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgAgenda.Rows[0].Clone();
+                    row.Cells[0].Value = reader.GetInt32(0);    //Id
+                    row.Cells[1].Value = reader.GetString(1);   //Nome
+                    row.Cells[2].Value = reader.GetString(2);   //Telefone
+                    row.Cells[3].Value = reader.GetString(3);   //Hora
+                    row.Cells[4].Value = reader.GetString(4);   //Data
+                    row.Cells[5].Value = reader.GetString(5);   //Endereço
+                    dgAgenda.Rows.Add(row);                     
+                }
+
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Não foi possível conectar-se ao banco de dados.");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void limparCampos()
+        {
+            tbId.Clear();
+            tbNome.Clear();
+            tbTelefone.Clear();
+            tbHora.Clear();
+            tbData.Clear();
+            tbEndereco.Clear();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "INSERT INTO agenda (nomeCliente,telefoneCliente,horaReparo,dataReparo,enderecoCliente) " +
+                    "VALUES('" + tbNome.Text + "', '" + tbTelefone.Text + "','" + tbHora.Text + "','" + tbData.Text + "','" + tbEndereco.Text + "')";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Reparo do(a) " + tbNome.Text + "[" + tbTelefone.Text + "] foi agendado para às " + tbHora.Text + " do dia " + tbData.Text + ".");
+                atualizarGrid();
+                limparCampos();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void calendario_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            tbData.Text = calendario.SelectionRange.Start.ToShortDateString();
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); 
+                comandoMySql.CommandText = "UPDATE agenda SET nomeCliente= '" + tbNome.Text + "', " +
+                    "telefoneCliente = '" + tbTelefone.Text + "', " +
+                    "horaReparo = '" + tbHora.Text + "', " +
+                    "dataReparo = '" + tbData.Text + "', " +
+                    "enderecoCliente = '" + tbEndereco.Text + "' " +
+                    " WHERE idCliente = " + tbId.Text + "";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); 
+                MessageBox.Show("ALTERADO com sucesso!"); 
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); 
+                comandoMySql.CommandText = "DELETE FROM agenda WHERE idCliente = " + tbId.Text + "";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); 
+                MessageBox.Show("ID " + tbId.Text + "(" + tbNome.Text + ", " + tbTelefone.Text + ", " + tbHora.Text + " - " + tbData.Text + ")" + " EXCLUÍDO com sucesso!");
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void dgAgenda_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgAgenda.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                dgAgenda.CurrentRow.Selected = true;
+                tbId.Text = dgAgenda.Rows[e.RowIndex].Cells["ColumnId"].FormattedValue.ToString();
+                tbNome.Text = dgAgenda.Rows[e.RowIndex].Cells["ColumnNome"].FormattedValue.ToString();
+                tbTelefone.Text = dgAgenda.Rows[e.RowIndex].Cells["ColumnTelefone"].FormattedValue.ToString();
+                tbHora.Text = dgAgenda.Rows[e.RowIndex].Cells["ColumnHora"].FormattedValue.ToString();
+                tbData.Text = dgAgenda.Rows[e.RowIndex].Cells["ColumnData"].FormattedValue.ToString();
+                tbEndereco.Text = dgAgenda.Rows[e.RowIndex].Cells["ColumnEndereco"].FormattedValue.ToString();
+            }
+        }
+    }
+}
